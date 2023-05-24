c343cf5dc411734cafaf24bf21304d77e785a071
Dmitry Zolotyh <zolotyh.dk@gmail.com>
2023-05-24 13:18:11 +0300

    Add Logging Aspect for all DAO methods

---
+++ src/main/java/com/zolotyh/spring/mvc_hibernate_aop/aspect/LoggingAspect.java
@@ -0,0 +1,22 @@
+package com.zolotyh.spring.mvc_hibernate_aop.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LoggingAspect {
+
+    @Around("execution(* com.zolotyh.spring.mvc_hibernate_aop.dao.*.*(..))")
+    public Object aroundAllRepositoryMethodsAdvice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable{
+        MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
+        String methodName = methodSignature.getName();
+        System.out.println("Begin of " + methodName);
+        Object targetMethodResult = proceedingJoinPoint.proceed();
+        System.out.println("End of " + methodName);
+        return targetMethodResult;
+    }
+}
